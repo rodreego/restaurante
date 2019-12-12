3b46ea0c4e721f5a6e359e9e0f2c49cec3c50bf9
Rodrigo Garcia <rodrigogarcia.vix@gmail.com>
2019-12-12 01:02:40 -0300

    inicio

---
+++ inc/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    database: 'saboroso',
+    password: 'caralho'
+  });
+
+  module.exports = connection;
