2441264bf84a489b0dcd0834a086970b1d12ff5d
Rodrigo Garcia <rodrigogarcia.vix@gmail.com>
2019-12-17 02:58:05 -0300

    enviando contato com metodo post

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var conn = require('./../inc/db')
 var express = require('express');
 var menus = require('./../inc/menus')
 var reservations = require('./../inc/reservations')
+var contacts = require('./../inc/contacts')
 var router = express.Router();
 
 /* GET home page. */
@@ -17,12 +18,32 @@ router.get('/', function(req,res,next){
 }) 
 
 router.get('/contacts', function(req,res,next){
+
+  contacts.render(req,res)
+
+});
+
+router.post('/contacts', function(req,res,next){
   
-  res.render('contacts', {
-    title: 'Contato - Restaurante Saboroso! ',
-    background: 'images/img_bg_3.jpg',
-    h1: 'Diga um oi!'
-  });
+  if(!req.body.name){
+    contacts.render(req,res,'digite o nome!')
+  }else if(!req.body.email){
+    contacts.render(req,res,'digite o e-mail!')
+  }else if(!req.body.message){
+    contacts.render(req,res,'digite a mensagem')
+  }else{
+    contacts.save(req.body).then(results=>{
+
+      req.body = {};
+
+      contacts.render(req,res,null,"contatato enviado com sucesso!")
+
+    }).catch(err=>{
+
+      contacts.render(res,req,err.message)
+
+    })
+  }
 
 });
 
