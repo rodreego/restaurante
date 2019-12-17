5b89f5276e813d304b2e3beb9f65da562a344504
Rodrigo Garcia <rodrigogarcia.vix@gmail.com>
2019-12-17 17:57:37 -0300

    express session com redis

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require("express-session")
+var RedisStore = require("connect-redis")(session)
+const redis = require('redis')
+let client = redis.createClient()
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  store: new RedisStore({client}),
+  secret: 'caralho',
+  resave: false
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
