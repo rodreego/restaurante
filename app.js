5f00988fde7dc454a3069300dea59de1e0e60851
Rodrigo Garcia <rodrigogarcia.vix@gmail.com>
2019-12-27 21:51:08 -0300

    enviando novo menu via post

--- app.js
+++ app.js
@@ -4,12 +4,35 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require("express-session")
+var formidable = require('formidable')
+var path = require('path')
 var RedisStore = require("connect-redis")(session)
 const redis = require('redis')
 let client = redis.createClient()
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var app = express();
+
+app.use(function(req,res,next){
+
+  if(req.method === 'POST'){
+
+    var form = formidable.IncomingForm({
+      uploadDir: path.join(__dirname, "/public/images"),
+      keepExtensions: true
+    })
+  
+    form.parse(req,function(err, fields, files){
+      req.fields = fields
+      req.files = files
+  
+      next()
+    })
+  }else{
+    next()
+  } 
+ 
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/admin.js
+++ routes/admin.js
@@ -90,7 +90,11 @@ router.get("/menus",function(req,res,next){
 
 router.post("/menus", function(req,res,next){
 
-    res.send(req.body);
+    menus.save(req.fields, req.files).then(results=>{
+        res.send(results)
+    }).catch(err=>{
+        res.send(err)
+    })
 
 });
 
