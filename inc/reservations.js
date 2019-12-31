8b69ba2967dce8b043e1f44a4572061bec675f76
Rodrigo Garcia <rodrigogarcia.vix@gmail.com>
2019-12-31 00:55:32 -0300

    terceira aula administração de reserva 192.MY32

--- inc/reservations.js
+++ inc/reservations.js
@@ -19,22 +19,47 @@ module.exports = {
 
         return new Promise((resolve, reject) => {
 
-            let date = fields.date.split('/')
+            if (fields.date.indexOf('/') > -1) {
 
-            fields.date = `${date[2]}-${date[1]}-${date[0]}`
+                let date = fields.date.split('/')
 
-            conn.query(`
-        
-                            INSERT INTO tb_reservations (name, email, people, date, time)
-                                VALUES (?, ?, ?, ?, ?)
-        
-                                    `, [
+                fields.date = `${date[2]}-${date[1]}-${date[0]}`
+
+            }
+
+            let query, params = [
                 fields.name,
                 fields.email,
                 fields.people,
                 fields.date,
                 fields.time
-            ], (err, results) => {
+            ]
+
+            if (parseInt(fields.id) > 0) {
+
+                query = `
+                    UPDATE tb_reservations 
+                    SET 
+                        name = ?,
+                        email = ?,
+                        people = ?,
+                        date = ?,
+                        time = ?
+
+                    WHERE id = ?
+                    `;
+
+                params.push(fields.id)
+
+            } else {
+
+                query = `
+                INSERT INTO tb_reservations (name, email, people, date, time)
+                VALUES (?, ?, ?, ?, ?)
+                `
+            }
+
+            conn.query(query, params, (err, results) => {
 
                 if (err) {
                     reject(err)
@@ -44,9 +69,28 @@ module.exports = {
 
             });
 
-
         });
 
+    },
+
+    getReservations() {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(
+                `SELECT * FROM tb_reservations ORDER BY date DESC
+                `, (err, results) => {
+
+                if (err) {
+                    reject(err);
+                }
+
+                resolve(results);
+            });
+
+        });
     }
+
+
 
 }
