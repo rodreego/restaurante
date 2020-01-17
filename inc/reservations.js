b6eaf5483bd27062a607e34bd6c8c907d2429703
Rodrigo Garcia <rodrigogarcia.vix@gmail.com>
2020-01-17 01:28:07 -0300

    gráfico tela de reservas

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,5 +1,8 @@
 var conn = require('./db')
 var Pagination = require("./../inc/Pagination")
+var moment = require("moment")
+
+
 module.exports = {
 
     render(req, res, error, success) {
@@ -132,8 +135,52 @@ module.exports = {
             })
         })
 
+    },
+
+    chart(req) {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+            SELECT 
+                CONCAT(YEAR(date), '-', MONTH(date)) AS date,
+                COUNT(*) AS total,
+                SUM(people) / COUNT(*) AS avg_people
+            FROM tb_reservations
+            WHERE
+                date BETWEEN ? AND ?
+            GROUP BY YEAR(date) DESC, MONTH(date) DESC
+            ORDER BY YEAR(date) DESC, MONTH(date) DESC;        
+            `, [
+
+                req.query.start,
+                req.query.end
+            ], (err, results) =>{
+
+                if (err) {
+                    reject(err)
+                } else {
+
+                    let months = [];
+                    let values = [];
+
+                    results.forEach(row => {
+
+                        months.push(moment(row.date).format('MMM YYYY'));
+                        values.push(row.total);
+
+                    });
+
+                    resolve({
+
+                        months,
+                        values
+                        
+                    });
+
+                }
+            })
+        })
     }
-
-
 
 }
